6355d14ec7e135eb3ea0dd60fb2a1d6209ae47c8
mustafa-azakli <mustafaazakli@yahoo.com>
2021-09-18 17:37:02 +0100

    mustafa azakli branch commit

---
+++ src/test/java/com/moneygaming/tests/mustafaazakliTest.java
@@ -0,0 +1,11 @@
+package com.moneygaming.tests;
+
+import org.testng.annotations.Test;
+
+public class mustafaazakliTest {
+
+    @Test
+    public void testOscar(){
+        System.out.println("my beautiful test");
+    }
+}
